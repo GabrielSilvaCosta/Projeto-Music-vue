097e54d427f021fdf782579e175bc82b14943938
GabrielSilvaCosta <bob@>
2024-05-01 14:36:44 -0300

    testes loading...

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,28 +3,37 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    },
+    {
+      files: ['src/components/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      globals: {
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        vi: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly'
+      }
     }
   ],
   env: {
     node: true
   },
   rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/no-reserved-component-names": "off"
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off'
   },
   parserOptions: {
     ecmaVersion: 'latest'
